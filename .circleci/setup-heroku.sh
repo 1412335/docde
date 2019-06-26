2bcff98397d2405c41804aeda4592a906c17cd35
namnnp <namnnp@vng.com.vn>
2019-06-26 15:25:45 +0700

    fix file sh

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-set -eu
-git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
-wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+# set -eu
+# git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
+# wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+# cat > ~/.netrc << EOF
+# machine api.heroku.com
+# login $HEROKU_LOGIN
+# password $HEROKU_API_KEY
+# machine git.heroku.com
+# login $HEROKU_LOGIN
+# password $HEROKU_API_KEY
+# EOF
+# chmod 600 ~/.netrc
+
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+# If you need access to the heroku CLI to run heroku commands in the deploy step add these lines:
+# mkdir -p /usr/local/lib /usr/local/bin
+# tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+# ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
 cat > ~/.netrc << EOF
 machine api.heroku.com
 login $HEROKU_LOGIN
@@ -10,4 +26,8 @@ machine git.heroku.com
 login $HEROKU_LOGIN
 password $HEROKU_API_KEY
 EOF
-chmod 600 ~/.netrc
+
+cat >> ~/.ssh/config << EOF
+VerifyHostKeyDNS yes
+StrictHostKeyChecking no
+EOF
